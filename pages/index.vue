40f0ee17519a395ffba2933d19724faf97d7c3d7
Iman Tabrizian <tabrizian@outlook.com>
2019-02-09 00:46:12 -0500

    Minor improvements

--- pages/index.vue
+++ pages/index.vue
@@ -2,14 +2,19 @@
   <v-layout column justify-center align-center>
     <v-flex xs12 sm8 md6>
       <v-card>
-        <v-card-title class="headline">Time Left Until Graduation!</v-card-title>
+        <v-card-title class="headline font-weight-bold">Time Left Until Graduation!</v-card-title>
         <v-card-text justify-center>
-           <countdown :time="gradDate" :interval="100">
+           <countdown :time="gradDate" class="title" :interval="100">
              <template slot-scope="props">Time Remaining：{{ props.days }} days, {{ props.hours }} hours, {{ props.minutes }} minutes, {{ props.seconds }} seconds.</template>
 </countdown>
        <v-progress-linear v-model="valueDeterminate"></v-progress-linear> {{ parseInt(this.valueDeterminate) }} %
 <p class="text-md-center">
-<i class="fas fa-skull fa-7x"></i></p>
+<i class="fas fa-skull fa-7x"></i>
+&nbsp;
+&nbsp;
+&nbsp;
+&nbsp;
+<i class="fas fa-graduation-cap fa-7x"></i></p>
 
         </v-card-text>
 
@@ -25,8 +30,8 @@
 <script>
 
 let now = new Date()
-let gradDate = new Date(2020, 12, 30)
-let beginDate = new Date(2019, 1, 1)
+let gradDate = new Date(2020, 11, 30)
+let beginDate = new Date(2019, 0, 1)
 let totalTime = gradDate - beginDate
 let passedTime = now - beginDate
 let remainingTime = gradDate - now
